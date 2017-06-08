5a0905a2bb6c6951d8f498c9d53d0c785c137d88
SleekPanther <macrobyte2@gmail.com>
2017-06-08 19:52:51 -0400

    counting inversions works

--- CountingInversions.java
+++ CountingInversions.java
@@ -1,16 +1,20 @@
 import java.util.*;
 
 public class CountingInversions {
-
+	private static ArrayList<ArrayList<String>> inversions = new ArrayList<ArrayList<String>>();
+	
 	public static void main(String[] args) {
-		int[] array = {3,2,1};
-		CountingInversions.mergeSort(array);
+//		int[] array = {1, 20, 6, 4, 5};	//5 inversions
+//		int[] array = {1,3,5,2,4,6};		//3 inversions
+//		int[] array = {3,2,1};	//3 inversions
+		int[] array = {5,4,3,2,1};	//10 inversions
+		System.out.println("Inversion count = "+CountingInversions.mergeSort(array));
 		System.out.println(Arrays.toString( array ) );
 	}
 
-	public static void mergeSort(int[] array) {
+	public static int mergeSort(int[] array) {
 		int[] tempArray = new int[array.length];
-		mergeSort(array, tempArray, 0, array.length - 1);
+		return mergeSort(array, tempArray, 0, array.length - 1);
 	}
 
 	/**
@@ -20,13 +24,15 @@ public class CountingInversions {
 	 * @param left the left-most index of the subarray.
 	 * @param right the right-most index of the subarray.
 	 */
-	private static void mergeSort(int[] array, int[] tempArray, int leftIndex, int rightIndex) {
+	private static int mergeSort(int[] array, int[] tempArray, int leftIndex, int rightIndex) {
+		int inversionCount = 0;
 		if( leftIndex < rightIndex ) {
 			int center = ( leftIndex + rightIndex ) / 2;
-			mergeSort( array, tempArray, leftIndex, center );
-			mergeSort( array, tempArray, center + 1, rightIndex );
-			merge( array, tempArray, leftIndex, center + 1, rightIndex );
+			inversionCount += mergeSort( array, tempArray, leftIndex, center );			//left inversions
+			inversionCount += mergeSort( array, tempArray, center + 1, rightIndex );	//right inversions
+			inversionCount += mergeAndCount( array, tempArray, leftIndex, center + 1, rightIndex );		//split inversions
 		}
+		return inversionCount;
 	}
 
 	/**
@@ -37,29 +43,39 @@ public class CountingInversions {
 	 * @param rightPos the index of the start of the second half.
 	 * @param rightEnd the right-most index of the subarray.
 	 */
-	private static void merge(int[] array, int[] tempArray, int leftPos, int rightPos, int rightEnd) {
-		
-		
+	private static int mergeAndCount(int[] array, int[] tempArray, int leftPos, int rightPos, int rightEnd) {
 		int leftEnd = rightPos - 1;
 		int tmpPos = leftPos;
 		int numElements = rightEnd - leftPos + 1;
 
+		int center = rightPos;
+
+		int inversionCount = 0;
+
 		// Main loop
-		while( leftPos <= leftEnd && rightPos <= rightEnd )
-			if( array[ leftPos ] <= array[rightPos])
+		while( leftPos <= leftEnd && rightPos <= rightEnd ){
+			if( array[ leftPos ] <= array[rightPos]){
 				tempArray[ tmpPos++ ] = array[ leftPos++ ];
-			else
+			}
+			else{	//Value from Right is greater than value from Left, we have as many inversions as elements remaining in left half
 				tempArray[ tmpPos++ ] = array[ rightPos++ ];
+				inversionCount += center-leftPos;		//Most important line.
+			}
+		}
 
-		while( leftPos <= leftEnd )    // Copy rest of first half
-		tempArray[ tmpPos++ ] = array[ leftPos++ ];
+		while( leftPos <= leftEnd ){		// Copy rest of first half
+			tempArray[ tmpPos++ ] = array[ leftPos++ ];
+		}
 
-		while( rightPos <= rightEnd )  // Copy rest of right half
-		tempArray[ tmpPos++ ] = array[ rightPos++ ];
+		while( rightPos <= rightEnd ){		// Copy rest of right half
+			tempArray[ tmpPos++ ] = array[ rightPos++ ];
+		}
 
-		// Copy tempArray back
-		for( int i = 0; i < numElements; i++, rightEnd-- )
+		for( int i = 0; i < numElements; i++, rightEnd-- ){		// Copy tempArray back
 			array[ rightEnd ] = tempArray[ rightEnd ];
+		}
+
+		return inversionCount;
 	}
 
 }
