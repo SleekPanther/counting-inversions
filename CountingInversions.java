c9c695f6ecec0a61272937332a7aaa8f2f8cb40d
SleekPanther <macrobyte2@gmail.com>
2017-06-08 18:11:16 -0400

    renaming

--- CountingInversions.java
+++ CountingInversions.java
@@ -9,56 +9,57 @@ public class CountingInversions {
 	}
 
 	public static void mergeSort(int[] array) {
-		int[] tmpArray = new int[array.length];
-
-		mergeSort( array, tmpArray, 0, array.length - 1 );
+		int[] tempArray = new int[array.length];
+		mergeSort(array, tempArray, 0, array.length - 1);
 	}
 
 	/**
 	 * Internal method that makes recursive calls.
 	 * @param a an array of Comparable items.
-	 * @param tmpArray an array to place the merged result.
+	 * @param tempArray an array to place the merged result.
 	 * @param left the left-most index of the subarray.
 	 * @param right the right-most index of the subarray.
 	 */
-	private static void mergeSort( int[] a, int[] tmpArray, int left, int right ) {
-		if( left < right ) {
-			int center = ( left + right ) / 2;
-			mergeSort( a, tmpArray, left, center );
-			mergeSort( a, tmpArray, center + 1, right );
-			merge( a, tmpArray, left, center + 1, right );
+	private static void mergeSort(int[] array, int[] tempArray, int leftIndex, int rightIndex) {
+		if( leftIndex < rightIndex ) {
+			int center = ( leftIndex + rightIndex ) / 2;
+			mergeSort( array, tempArray, leftIndex, center );
+			mergeSort( array, tempArray, center + 1, rightIndex );
+			merge( array, tempArray, leftIndex, center + 1, rightIndex );
 		}
 	}
 
 	/**
 	 * Internal method that merges two sorted halves of a subarray.
 	 * @param a an array of Comparable items.
-	 * @param tmpArray an array to place the merged result.
+	 * @param tempArray an array to place the merged result.
 	 * @param leftPos the left-most index of the subarray.
 	 * @param rightPos the index of the start of the second half.
 	 * @param rightEnd the right-most index of the subarray.
 	 */
-	private static 	void merge( int[] a, int[] tmpArray, int leftPos, int rightPos, int rightEnd ) {
+	private static void merge(int[] array, int[] tempArray, int leftPos, int rightPos, int rightEnd) {
+		
+		
 		int leftEnd = rightPos - 1;
 		int tmpPos = leftPos;
 		int numElements = rightEnd - leftPos + 1;
 
 		// Main loop
 		while( leftPos <= leftEnd && rightPos <= rightEnd )
-			if( a[ leftPos ] <= a[rightPos])
-				tmpArray[ tmpPos++ ] = a[ leftPos++ ];
+			if( array[ leftPos ] <= array[rightPos])
+				tempArray[ tmpPos++ ] = array[ leftPos++ ];
 			else
-				tmpArray[ tmpPos++ ] = a[ rightPos++ ];
+				tempArray[ tmpPos++ ] = array[ rightPos++ ];
 
 		while( leftPos <= leftEnd )    // Copy rest of first half
-		tmpArray[ tmpPos++ ] = a[ leftPos++ ];
+		tempArray[ tmpPos++ ] = array[ leftPos++ ];
 
 		while( rightPos <= rightEnd )  // Copy rest of right half
-		tmpArray[ tmpPos++ ] = a[ rightPos++ ];
+		tempArray[ tmpPos++ ] = array[ rightPos++ ];
 
-		// Copy tmpArray back
+		// Copy tempArray back
 		for( int i = 0; i < numElements; i++, rightEnd-- )
-			a[ rightEnd ] = tmpArray[ rightEnd ];
+			array[ rightEnd ] = tempArray[ rightEnd ];
 	}
 
 }
