2cfe3068e3db6ab1f1c1870dca0c3e46f627713b
SleekPanther <macrobyte2@gmail.com>
2017-06-09 07:22:34 -0400

    find the actual inversions

--- CountingInversions.java
+++ CountingInversions.java
@@ -1,15 +1,25 @@
 import java.util.*;
 
 public class CountingInversions {
-	private static ArrayList<ArrayList<String>> inversions = new ArrayList<ArrayList<String>>();
+	private static ArrayList<ArrayList<Integer>> inversions = new ArrayList<ArrayList<Integer>>();
 	
 	public static void main(String[] args) {
-//		int[] array = {1, 20, 6, 4, 5};	//5 inversions
-//		int[] array = {1,3,5,2,4,6};		//3 inversions
+		CountingInversions inversionCounter = new CountingInversions();
+		
+		int[] array = {1, 20, 6, 4, 5};	//5 inversions
+		// int[] array = {1,3,5,2,4,6};	//3 inversions
 //		int[] array = {3,2,1};	//3 inversions
-		int[] array = {5,4,3,2,1};	//10 inversions
+//		int[] array = {4,3,2,1};	//6 inversions
+//		int[] array = {5,4,3,2,1};	//10 inversions
+		// int[] array = {2, 4, 1, 3, 5};	//3 inversions
+		// int[] array = {1, 2, 3, 4};	//0 inversions
+		// int[] array = {3, 3, 3, 3};	//0 inversions
 		System.out.println("Inversion count = "+CountingInversions.mergeSort(array));
 		System.out.println(Arrays.toString( array ) );
+		System.out.println("Actual Inversions");
+		for(ArrayList<Integer> inversion : inversions){
+			System.out.println(inversion);
+		}
 	}
 
 	public static int mergeSort(int[] array) {
@@ -44,31 +54,35 @@ public class CountingInversions {
 	 * @param rightEnd the right-most index of the subarray.
 	 */
 	private static int mergeAndCount(int[] array, int[] tempArray, int leftPos, int rightPos, int rightEnd) {
-		int leftEnd = rightPos - 1;
-		int tmpPos = leftPos;
-		int numElements = rightEnd - leftPos + 1;
+		int inversionCount = 0;
 
 		int center = rightPos;
-
-		int inversionCount = 0;
+		
+		int leftEnd = rightPos - 1;
+		int tempPos = leftPos;
+		int numElements = rightEnd - leftPos + 1;
 
 		// Main loop
 		while( leftPos <= leftEnd && rightPos <= rightEnd ){
-			if( array[ leftPos ] <= array[rightPos]){
-				tempArray[ tmpPos++ ] = array[ leftPos++ ];
+			if( array[leftPos] <= array[rightPos]){
+				tempArray[ tempPos++ ] = array[ leftPos++ ];
 			}
 			else{	//Value from Right is greater than value from Left, we have as many inversions as elements remaining in left half
-				tempArray[ tmpPos++ ] = array[ rightPos++ ];
-				inversionCount += center-leftPos;		//Most important line.
+				tempArray[ tempPos++ ] = array[ rightPos++ ];
+				inversionCount += center-leftPos;		//Most important line
+				//Loop over the rest of the left half & find the actual numbers causing inversions with 1 constant number from the right half
+				for(int i=leftPos; i<=leftEnd; i++){		//Bounds loop over the left half
+					inversions.add(new ArrayList<Integer>(Arrays.asList(array[i], array[rightPos-1])));		//rightPos-1 because rightPos was incremented in line "tempArray[ tempPos++ ] = array[ rightPos++ ];", so the actual number from the right half is is 1 less then where the variable currently points
+				}
 			}
 		}
-
+		
 		while( leftPos <= leftEnd ){		// Copy rest of first half
-			tempArray[ tmpPos++ ] = array[ leftPos++ ];
+			tempArray[ tempPos++ ] = array[ leftPos++ ];
 		}
 
 		while( rightPos <= rightEnd ){		// Copy rest of right half
-			tempArray[ tmpPos++ ] = array[ rightPos++ ];
+			tempArray[ tempPos++ ] = array[ rightPos++ ];
 		}
 
 		for( int i = 0; i < numElements; i++, rightEnd-- ){		// Copy tempArray back
